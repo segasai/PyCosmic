11e44100f556f5570380a98ef0a2127d60cd865a
brandherd <bhuseman@eso.org>
2014-05-22 22:01:44 +0200

    updated the setup script to v0.3 and to new email adress

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 from distutils.core import setup
 setup(name='PyCosmic',
-      version='0.1',
+      version='0.3',
       description='Cosmic ray rejection algorithm on single frame CCD images',
       author='Bernd Husemann',
-      author_email='bhusemann@aip.de',
+      author_email='bhuseman@eso.org',
       packages=['PyCosmic','PyCosmic.lib'],
       url='http://sourceforge.net/projects/pycosmic/',
       requires=['scipy','numpy','pyfits'],
